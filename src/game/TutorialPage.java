e07eca746d8225ea384df238af329f0d6997a756
Naveedkk2060 <74552931+Naveedkk2060@users.noreply.github.com>
2022-01-24 22:42:29 -0500

    Update

--- src/game/TutorialPage.java
+++ src/game/TutorialPage.java
@@ -1,7 +1,6 @@
 package game;
 
-import java.awt.Color;
-import java.awt.FontFormatException;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -14,16 +13,32 @@ public class TutorialPage implements ActionListener {
 
 	JFrame f;
 	JButton back;
-	ImageIcon background;
-	JLabel backgroundLabel;
+	ImageIcon background, Title;
+	JLabel backgroundLabel, titleLabel;
+	
 	TutorialPage() throws FontFormatException, IOException {
+		
 		background = new ImageIcon(getClass().getResource("/mainMenuBackground.jpg"));
 		backgroundLabel = new JLabel(background);
 		f = new JFrame("Our Lost Friend - Tutorial");
+		f.setSize(1600, 900);
 		f.add(backgroundLabel);
 		back = new JButton("Back to Main Menu");
-
-
+		back.setBounds(600, 500, 300, 60);
+		backgroundLabel.setSize(1600, 900);
+		Title = new ImageIcon(this.getClass().getResource("/title.png"));
+		titleLabel = new JLabel(Title);
+		makeLabel(titleLabel);
+		
+		
+		
+		makeButton(back);
+		f.setLayout(null);
+		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.add(titleLabel);
+		f.add(backgroundLabel);
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -37,6 +52,21 @@ public class TutorialPage implements ActionListener {
 			}
 		}
 
+	}
+	
+	public void makeButton(JButton b) {
+		b.setForeground(Color.white);
+		b.setBackground(new Color(36, 100, 187));
+		b.addActionListener(this);
+		b.setFocusable(false);
+		b.setOpaque(false);
+		b.setContentAreaFilled(false);
+		b.setBorderPainted(false);
+		backgroundLabel.add(b);
+	}
+	
+	public void makeLabel(JLabel l) {
+		l.setForeground(Color.white);
 	}
 
 }
