35311d13938a1d7fd636a484a415f65692654753
Ian456s <iantang9000@gmail.com>
2022-01-24 23:14:47 -0500

    Updated Tutorial Page

--- src/game/TutorialPage.java
+++ src/game/TutorialPage.java
@@ -15,39 +15,36 @@ public class TutorialPage implements ActionListener {
 	JButton back;
 	ImageIcon background, Title;
 	JLabel backgroundLabel, titleLabel, text;
-	Font buttonFont, biggerFont;
+	Font buttonFont, biggerFont, textFont;
 	
 	TutorialPage() throws FontFormatException, IOException {
 		buttonFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("/textFont.TTF"));
-        biggerFont = buttonFont.deriveFont(Font.BOLD, 20f);
+        textFont = buttonFont.deriveFont(Font.BOLD, 24f);
+		biggerFont = buttonFont.deriveFont(Font.BOLD, 32f);
 		Title = new ImageIcon(this.getClass().getResource("/title.png"));
 		background = new ImageIcon(getClass().getResource("/mainMenuBackground.jpg"));
 		backgroundLabel = new JLabel(background);
+		backgroundLabel.setSize(1600, 900);
 		titleLabel = new JLabel(Title);
 		titleLabel.setSize(980,60);
 		titleLabel.setBounds(180, 100, 1158, 97);
+		makeLabel(titleLabel);
 		text = new JLabel("In this game the objective is to get to the end of the level. Press the a and d keys to move and press the space bar to jump.");
-		text.setBounds(300, 250, 1700, 180);
+		text.setBounds(280, 220, 1300, 180);
+		makeLabel(text);
 		f = new JFrame("Our Lost Friend - Tutorial");
 		f.setSize(1600, 900);
 		f.add(backgroundLabel);
 		back = new JButton("Back to Main Menu");
-		back.setBounds(800, 700, 500, 260);
-		backgroundLabel.setSize(1600, 900);
 		
-		makeLabel(text);
-		makeLabel(titleLabel);
-		
-		
-		
-		makeButton(back);
+		makeBackButton(back);
+		f.add(text);
+		f.add(titleLabel);
+		f.add(backgroundLabel);
 		f.setLayout(null);
 		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		f.setVisible(true);
 		f.setLocationRelativeTo(null);
-		f.add(text);
-		f.add(titleLabel);
-		f.add(backgroundLabel);
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -63,8 +60,8 @@ public class TutorialPage implements ActionListener {
 
 	}
 	
-	public void makeButton(JButton b) {
-		b.setFont(buttonFont);
+	private void makeBackButton(JButton b) {
+		b.setFont(biggerFont);
 		b.setForeground(Color.white);
 		b.setBackground(new Color(36, 100, 187));
 		b.addActionListener(this);
@@ -72,11 +69,12 @@ public class TutorialPage implements ActionListener {
 		b.setOpaque(false);
 		b.setContentAreaFilled(false);
 		b.setBorderPainted(false);
+		b.setBounds(360, 700, 500, 100);
 		backgroundLabel.add(b);
 	}
 	
 	public void makeLabel(JLabel l) {
-		l.setFont(biggerFont);
+		l.setFont(textFont);
 		l.setForeground(Color.white);
 	}
 
