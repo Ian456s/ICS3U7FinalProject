c3134e2fe32a999fa5516732b14e3d81337e29c4
Ian456s <iantang9000@gmail.com>
2022-01-27 12:42:02 -0500

    updated files

--- src/game/physics/Collision.java
+++ src/game/physics/Collision.java
@@ -6,10 +6,21 @@ import game.objects.Block;
 import game.objects.MovingBlock;
 
 public class Collision {
+	/**
+	 * playerBlock method for checking if the player is making contact with a block
+	 * @param p - the point that is being checked
+	 * @param b - the block that is being checked
+	 * @return a boolean depending on whether or not the point is within the block.
+	 */
 	public static boolean playerBlock(Point p, Block b) {
 		return b.contains(p);
 	}
-	
+	/**
+	 * playerMovingBlock method for checking if the player is currently making contact with a moving block
+	 * @param p - the point of which is being checked
+	 * @param b - the block that is being checked
+	 * @return a boolean depending on whether or not the point is within the block.
+	 */
 	public static boolean playerMovingBlock(Point p, MovingBlock b) {
 		return b.contains(p);
 	}
--- src/game/sprites/Images.java
+++ src/game/sprites/Images.java
@@ -1,15 +1,19 @@
 package game.sprites;
 
 import java.awt.image.BufferedImage;
+/**
+ * Images class for rendering images for our game
+ */
 import java.io.*;
 import javax.imageio.*;
 public class Images {
 	public static BufferedImage[] blocks;
 	public Images() {
-		blocks = new BufferedImage[2];
+		blocks = new BufferedImage[3];
 		try {
 			blocks[0] = ImageIO.read(getClass().getResourceAsStream("/Blocks/grass.jpg"));
 			blocks[1] = ImageIO.read(getClass().getResourceAsStream("/Blocks/movingBlock.png")); 
+			blocks[2] = ImageIO.read(getClass().getResourceAsStream("/Blocks/waterBlock.gif"));
 		} catch (IOException e) {		
 			e.printStackTrace();
 		}
